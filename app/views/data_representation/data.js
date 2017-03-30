4e529862521c60604606fe13f0a7ee5054c4c683
Omar Almootassem <218omar.a@gmail.com>
2017-03-30 14:49:00 -0400

    added max and min charts

--- app/views/data_representation/data.js
+++ app/views/data_representation/data.js
@@ -77,6 +77,26 @@ angular.module('CloudApp.data_representation', ['ngRoute'])
 	];
 	$scope.options = {legend: {display: true, position: "bottom"}};
 
+	$scope.mongoData = [
+		[0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0]
+	];
+
+	$scope.dynamoData = [
+		[0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0]
+	];
+
+	$scope.fireData = [
+		[0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0]
+	];
+
+	$scope.couchData = [
+		[0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0]
+	];
+
 	firebase.database().ref('test_history/post/mongodb/small').on('value', function(snapshot){
 		var average = 0, count = 0;
 		snapshot.forEach(function(childSnapshot){
@@ -339,6 +359,393 @@ angular.module('CloudApp.data_representation', ['ngRoute'])
 			average = average/count;
 		$scope.data[3][5] = average.toFixed(2);
 		$scope.$applyAsync();
+	});
+
+
+
+
+	firebase.database().ref('test_history/post/mongodb/small').on('value', function(snapshot){
+		var lowest = 9999;
+		var highest = 0;
+		var tempL = 0, tempH = 0;
+		snapshot.forEach(function(childSnapshot){
+			if (childSnapshot.val().time_ms > highest){
+				highest = childSnapshot.val().time_ms;
+			}
+			if (childSnapshot.val().time_ms < lowest){
+				lowest = childSnapshot.val().time_ms;
+			}
+			$scope.mongoData[0][0] = highest;
+			$scope.mongoData[1][0] = lowest;
+		});
+	});
+
+	firebase.database().ref('test_history/post/mongodb/large').on('value', function(snapshot){
+		var lowest = 9999;
+		var highest = 0;
+		var tempL = 0, tempH = 0;
+		snapshot.forEach(function(childSnapshot){
+			if (childSnapshot.val().time_ms > highest){
+				highest = childSnapshot.val().time_ms;
+			}
+			if (childSnapshot.val().time_ms < lowest){
+				lowest = childSnapshot.val().time_ms;
+			}
+			$scope.mongoData[0][1] = highest;
+			$scope.mongoData[1][1] = lowest;
+		});
+	});
+
+	firebase.database().ref('test_history/get/mongodb/small').on('value', function(snapshot){
+		var lowest = 9999;
+		var highest = 0;
+		var tempL = 0, tempH = 0;
+		snapshot.forEach(function(childSnapshot){
+			if (childSnapshot.val().time_ms > highest){
+				highest = childSnapshot.val().time_ms;
+			}
+			if (childSnapshot.val().time_ms < lowest){
+				lowest = childSnapshot.val().time_ms;
+			}
+			$scope.mongoData[0][2] = highest;
+			$scope.mongoData[1][2] = lowest;
+		});
+	});
+
+	firebase.database().ref('test_history/get/mongodb/large').on('value', function(snapshot){
+		var lowest = 9999;
+		var highest = 0;
+		var tempL = 0, tempH = 0;
+		snapshot.forEach(function(childSnapshot){
+			if (childSnapshot.val().time_ms > highest){
+				highest = childSnapshot.val().time_ms;
+			}
+			if (childSnapshot.val().time_ms < lowest){
+				lowest = childSnapshot.val().time_ms;
+			}
+			$scope.mongoData[0][3] = highest;
+			$scope.mongoData[1][3] = lowest;
+		});
+	});
+
+	firebase.database().ref('test_history/update/mongodb/small').on('value', function(snapshot){
+		var lowest = 9999;
+		var highest = 0;
+		var tempL = 0, tempH = 0;
+		snapshot.forEach(function(childSnapshot){
+			if (childSnapshot.val().time_ms > highest){
+				highest = childSnapshot.val().time_ms;
+			}
+			if (childSnapshot.val().time_ms < lowest){
+				lowest = childSnapshot.val().time_ms;
+			}
+			$scope.mongoData[0][4] = highest;
+			$scope.mongoData[1][4] = lowest;
+		});
+	});
+
+	firebase.database().ref('test_history/update/mongodb/large').on('value', function(snapshot){
+		var lowest = 9999;
+		var highest = 0;
+		var tempL = 0, tempH = 0;
+		snapshot.forEach(function(childSnapshot){
+			if (childSnapshot.val().time_ms > highest){
+				highest = childSnapshot.val().time_ms;
+			}
+			if (childSnapshot.val().time_ms < lowest){
+				lowest = childSnapshot.val().time_ms;
+			}
+			$scope.mongoData[0][5] = highest;
+			$scope.mongoData[1][5] = lowest;
+		});
+	});
+
+	firebase.database().ref('test_history/post/dynamodb/small').on('value', function(snapshot){
+		var lowest = 9999;
+		var highest = 0;
+		var tempL = 0, tempH = 0;
+		snapshot.forEach(function(childSnapshot){
+			if (childSnapshot.val().time_ms > highest){
+				highest = childSnapshot.val().time_ms;
+			}
+			if (childSnapshot.val().time_ms < lowest){
+				lowest = childSnapshot.val().time_ms;
+			}
+			$scope.dynamoData[0][0] = highest;
+			$scope.dynamoData[1][0] = lowest;
+		});
+	});
+
+	firebase.database().ref('test_history/post/dynamodb/large').on('value', function(snapshot){
+		var lowest = 9999;
+		var highest = 0;
+		var tempL = 0, tempH = 0;
+		snapshot.forEach(function(childSnapshot){
+			if (childSnapshot.val().time_ms > highest){
+				highest = childSnapshot.val().time_ms;
+			}
+			if (childSnapshot.val().time_ms < lowest){
+				lowest = childSnapshot.val().time_ms;
+			}
+			$scope.dynamoData[0][1] = highest;
+			$scope.dynamoData[1][1] = lowest;
+		});
+	});
+
+	firebase.database().ref('test_history/get/dynamodb/small').on('value', function(snapshot){
+		var lowest = 9999;
+		var highest = 0;
+		var tempL = 0, tempH = 0;
+		snapshot.forEach(function(childSnapshot){
+			if (childSnapshot.val().time_ms > highest){
+				highest = childSnapshot.val().time_ms;
+			}
+			if (childSnapshot.val().time_ms < lowest){
+				lowest = childSnapshot.val().time_ms;
+			}
+			$scope.dynamoData[0][2] = highest;
+			$scope.dynamoData[1][2] = lowest;
+		});
+	});
+
+	firebase.database().ref('test_history/get/dynamodb/large').on('value', function(snapshot){
+		var lowest = 9999;
+		var highest = 0;
+		var tempL = 0, tempH = 0;
+		snapshot.forEach(function(childSnapshot){
+			if (childSnapshot.val().time_ms > highest){
+				highest = childSnapshot.val().time_ms;
+			}
+			if (childSnapshot.val().time_ms < lowest){
+				lowest = childSnapshot.val().time_ms;
+			}
+			$scope.dynamoData[0][3] = highest;
+			$scope.dynamoData[1][3] = lowest;
+		});
+	});
+
+	firebase.database().ref('test_history/update/dynamodb/small').on('value', function(snapshot){
+		var lowest = 9999;
+		var highest = 0;
+		var tempL = 0, tempH = 0;
+		snapshot.forEach(function(childSnapshot){
+			if (childSnapshot.val().time_ms > highest){
+				highest = childSnapshot.val().time_ms;
+			}
+			if (childSnapshot.val().time_ms < lowest){
+				lowest = childSnapshot.val().time_ms;
+			}
+			$scope.dynamoData[0][4] = highest;
+			$scope.dynamoData[1][4] = lowest;
+		});
+	});
+
+	firebase.database().ref('test_history/update/dynamodb/large').on('value', function(snapshot){
+		var lowest = 9999;
+		var highest = 0;
+		var tempL = 0, tempH = 0;
+		snapshot.forEach(function(childSnapshot){
+			if (childSnapshot.val().time_ms > highest){
+				highest = childSnapshot.val().time_ms;
+			}
+			if (childSnapshot.val().time_ms < lowest){
+				lowest = childSnapshot.val().time_ms;
+			}
+			$scope.dynamoData[0][5] = highest;
+			$scope.dynamoData[1][5] = lowest;
+		});
+	});
+
+	firebase.database().ref('test_history/post/firebase/small').on('value', function(snapshot){
+		var lowest = 9999;
+		var highest = 0;
+		var tempL = 0, tempH = 0;
+		snapshot.forEach(function(childSnapshot){
+			if (childSnapshot.val().time_ms > highest){
+				highest = childSnapshot.val().time_ms;
+			}
+			if (childSnapshot.val().time_ms < lowest){
+				lowest = childSnapshot.val().time_ms;
+			}
+			$scope.fireData[0][0] = highest;
+			$scope.fireData[1][0] = lowest;
+		});
+	});
+
+	firebase.database().ref('test_history/post/firebase/large').on('value', function(snapshot){
+		var lowest = 9999;
+		var highest = 0;
+		var tempL = 0, tempH = 0;
+		snapshot.forEach(function(childSnapshot){
+			if (childSnapshot.val().time_ms > highest){
+				highest = childSnapshot.val().time_ms;
+			}
+			if (childSnapshot.val().time_ms < lowest){
+				lowest = childSnapshot.val().time_ms;
+			}
+			$scope.fireData[0][1] = highest;
+			$scope.fireData[1][1] = lowest;
+		});
+	});
+
+	firebase.database().ref('test_history/get/firebase/small').on('value', function(snapshot){
+		var lowest = 9999;
+		var highest = 0;
+		var tempL = 0, tempH = 0;
+		snapshot.forEach(function(childSnapshot){
+			if (childSnapshot.val().time_ms > highest){
+				highest = childSnapshot.val().time_ms;
+			}
+			if (childSnapshot.val().time_ms < lowest){
+				lowest = childSnapshot.val().time_ms;
+			}
+			$scope.fireData[0][2] = highest;
+			$scope.fireData[1][2] = lowest;
+		});
+	});
+
+	firebase.database().ref('test_history/get/firebase/large').on('value', function(snapshot){
+		var lowest = 9999;
+		var highest = 0;
+		var tempL = 0, tempH = 0;
+		snapshot.forEach(function(childSnapshot){
+			if (childSnapshot.val().time_ms > highest){
+				highest = childSnapshot.val().time_ms;
+			}
+			if (childSnapshot.val().time_ms < lowest){
+				lowest = childSnapshot.val().time_ms;
+			}
+			$scope.fireData[0][3] = highest;
+			$scope.fireData[1][3] = lowest;
+		});
+	});
+
+	firebase.database().ref('test_history/update/firebase/small').on('value', function(snapshot){
+		var lowest = 9999;
+		var highest = 0;
+		var tempL = 0, tempH = 0;
+		snapshot.forEach(function(childSnapshot){
+			if (childSnapshot.val().time_ms > highest){
+				highest = childSnapshot.val().time_ms;
+			}
+			if (childSnapshot.val().time_ms < lowest){
+				lowest = childSnapshot.val().time_ms;
+			}
+			$scope.fireData[0][4] = highest;
+			$scope.fireData[1][4] = lowest;
+		});
+	});
+
+	firebase.database().ref('test_history/update/firebase/large').on('value', function(snapshot){
+		var lowest = 9999;
+		var highest = 0;
+		var tempL = 0, tempH = 0;
+		snapshot.forEach(function(childSnapshot){
+			if (childSnapshot.val().time_ms > highest){
+				highest = childSnapshot.val().time_ms;
+			}
+			if (childSnapshot.val().time_ms < lowest){
+				lowest = childSnapshot.val().time_ms;
+			}
+			$scope.fireData[0][5] = highest;
+			$scope.fireData[1][5] = lowest;
+		});
+	});
+
+	firebase.database().ref('test_history/post/couchdb/small').on('value', function(snapshot){
+		var lowest = 9999;
+		var highest = 0;
+		var tempL = 0, tempH = 0;
+		snapshot.forEach(function(childSnapshot){
+			if (childSnapshot.val().time_ms > highest){
+				highest = childSnapshot.val().time_ms;
+			}
+			if (childSnapshot.val().time_ms < lowest){
+				lowest = childSnapshot.val().time_ms;
+			}
+			$scope.couchData[0][0] = highest;
+			$scope.couchData[1][0] = lowest;
+		});
+	});
+
+	firebase.database().ref('test_history/post/couchdb/large').on('value', function(snapshot){
+		var lowest = 9999;
+		var highest = 0;
+		var tempL = 0, tempH = 0;
+		snapshot.forEach(function(childSnapshot){
+			if (childSnapshot.val().time_ms > highest){
+				highest = childSnapshot.val().time_ms;
+			}
+			if (childSnapshot.val().time_ms < lowest){
+				lowest = childSnapshot.val().time_ms;
+			}
+			$scope.couchData[0][1] = highest;
+			$scope.couchData[1][1] = lowest;
+		});
+	});
+
+	firebase.database().ref('test_history/get/couchdb/small').on('value', function(snapshot){
+		var lowest = 9999;
+		var highest = 0;
+		var tempL = 0, tempH = 0;
+		snapshot.forEach(function(childSnapshot){
+			if (childSnapshot.val().time_ms > highest){
+				highest = childSnapshot.val().time_ms;
+			}
+			if (childSnapshot.val().time_ms < lowest){
+				lowest = childSnapshot.val().time_ms;
+			}
+			$scope.couchData[0][2] = highest;
+			$scope.couchData[1][2] = lowest;
+		});
+	});
+
+	firebase.database().ref('test_history/get/couchdb/large').on('value', function(snapshot){
+		var lowest = 9999;
+		var highest = 0;
+		var tempL = 0, tempH = 0;
+		snapshot.forEach(function(childSnapshot){
+			if (childSnapshot.val().time_ms > highest){
+				highest = childSnapshot.val().time_ms;
+			}
+			if (childSnapshot.val().time_ms < lowest){
+				lowest = childSnapshot.val().time_ms;
+			}
+			$scope.couchData[0][3] = highest;
+			$scope.couchData[1][3] = lowest;
+		});
+	});
+
+	firebase.database().ref('test_history/update/couchdb/small').on('value', function(snapshot){
+		var lowest = 9999;
+		var highest = 0;
+		var tempL = 0, tempH = 0;
+		snapshot.forEach(function(childSnapshot){
+			if (childSnapshot.val().time_ms > highest){
+				highest = childSnapshot.val().time_ms;
+			}
+			if (childSnapshot.val().time_ms < lowest){
+				lowest = childSnapshot.val().time_ms;
+			}
+			$scope.couchData[0][4] = highest;
+			$scope.couchData[1][4] = lowest;
+		});
+	});
+
+	firebase.database().ref('test_history/update/couchdb/large').on('value', function(snapshot){
+		var lowest = 9999;
+		var highest = 0;
+		var tempL = 0, tempH = 0;
+		snapshot.forEach(function(childSnapshot){
+			if (childSnapshot.val().time_ms > highest){
+				highest = childSnapshot.val().time_ms;
+			}
+			if (childSnapshot.val().time_ms < lowest){
+				lowest = childSnapshot.val().time_ms;
+			}
+			$scope.couchData[0][5] = highest;
+			$scope.couchData[1][5] = lowest;
+		});
 	});
 
 
